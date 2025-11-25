3d79a085e27e1cd29ba54e8fdc137b1e6079cc09
Claudio Volz <badunaarea@hotmail.com>
2025-11-25 16:43:56 -0300

    ✨ implementa funções básicas da calculadora

--- calculator.js
+++ calculator.js
@@ -1,16 +1,34 @@
 // retorna o valor da soma entre os dois parâmetros
-function sum(v1, v2) {}
+function sum(v1, v2) {
+    return v1 + v2;
+}
 
 // retorna o valor da subtração entre os dois parâmetros
-function sub(v1, v2) {}
+function sub(v1, v2) {
+    return v1 - v2;
+}
 
 // retorna o valor da divisão entre os dois parâmetros
-function div(v1, v2) {}
+function div(v1, v2) {
+    if (v2 === 0) {
+        return "Erro: divisão por zero";
+    }
+    return v1 / v2;
+}
 
 // retorna o valor da multiplicação entre os dois parâmetros
-function mult(v1, v2) {}
+function mult(v1, v2) {
+    return v1 * v2;
+}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {
+    if (v1 < 0) {
+        return "Erro: número negativo não possui raiz real";
+    }
+    return Math.sqrt(v1);
+}
 
-console.log(sum(v1, v2));
+// Apenas para teste (exemplo)
+// Você pode remover essa linha depois ou ajustar
+console.log(sum(10, 5));
